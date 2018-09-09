263d84a59aace234ce87cb58d1a6cd5202b48551
Rich Jones <rich@anomos.info>
2018-09-09 19:49:31 -0400

    adds --input

--- chords2midi/c2m.py
+++ chords2midi/c2m.py
@@ -34,6 +34,7 @@ class Chords2Midi(object):
         parser.add_argument('progression', metavar='U', type=str, nargs='*', help=help_message)
         parser.add_argument('-b', '--bpm', type=int, default=160, help='Set the BPM (default 160)')
         parser.add_argument('-t', '--octave', type=int, default=4, help='Set the octave (default 4)')
+        parser.add_argument('-i', '--input', type=str, default=None, help='Read from an input file.')
         parser.add_argument('-k', '--key', type=str, default='C', help='Set the key (default C)')
         parser.add_argument('-d', '--duration', type=float, default=1.0, help='Set the chord duraction (default 1)')
         parser.add_argument('-o', '--output', type=str, help='Set the output file path. Default is the current key and progression in the current location.')
@@ -49,13 +50,19 @@ class Chords2Midi(object):
             return
 
         # Support `c2m I III V and `c2m I,III,V` formats.
-        if len(self.vargs['progression']) < 1:
-            print("You need to supply a progression! (ex I V vi IV)")
-            return
-        if len(self.vargs['progression']) < 2:
-            progression = self.vargs['progression'][0].split(',')
+        if not self.vargs['input']:
+            if len(self.vargs['progression']) < 1:
+                print("You need to supply a progression! (ex I V vi IV)")
+                return
+            if len(self.vargs['progression']) < 2:
+                progression = self.vargs['progression'][0].split(',')
+            else:
+                progression = self.vargs['progression']
         else:
-            progression = self.vargs['progression']
+            with open(self.vargs['input']) as fn:
+                content = ''.join(fn.readlines()).strip()
+                content = content.replace('\n', ' ').replace(',', '  ')
+                progression = content.split(' ')
 
         track    = 0
         channel  = 0
@@ -77,6 +84,8 @@ class Chords2Midi(object):
 
         if self.vargs['output']:
             filename = self.vargs['output']
+        elif self.vargs['input']:
+            filename = self.vargs['input'].replace('.txt', '.mid')
         else:
             filename = self.vargs['key'] + '-'  + '-'.join(progression) + '-' + str(self.vargs['bpm']) + '.mid'
             if os.path.exists(filename):
