16c082758743cce7fa4818391ce5fca07f007c9f
Rich Jones <rich@anomos.info>
2018-09-08 20:40:58 -0400

    Initial commit and working version

---
+++ README.md
@@ -0,0 +1,39 @@
+# chord2midi
+
+Given it a chord progression, get a MIDI file!
+
+## Installation
+
+    $ pip install chord2midi
+
+## Usage
+
+    $ p chords2midi/c2m.py I V vi IV --key C
+    $ ls
+    C-I-V-vi-IV.mid
+
+Further options:
+
+```
+usage: c2m.py [-h] [-i] [-b BPM] [-t OCTAVE] [-k KEY] [-d DURATION] [-v]
+              [U [U ...]]
+
+chords2midi - Create MIDI files from written chord progressions.
+
+positional arguments:
+  U                     Please supply chord progression!. See --help for more
+                        options.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i, --install         Install into Ableton directory
+  -b BPM, --bpm BPM     Set the BPM (default 160)
+  -t OCTAVE, --octave OCTAVE
+                        Set the octave (default 4)
+  -k KEY, --key KEY     Set the key (default C)
+  -d DURATION, --duration DURATION
+                        Set the chord duraction (default 1)
+  -v, --version         Display the current version of chords2midi
+```
+
+Rich Jones, 2018. MIT.
---
+++ chords2midi/c2m.py
@@ -0,0 +1,100 @@
+# c2m.py - chords2midi
+
+import argparse
+import os
+import pychord
+import time
+
+from midiutil import MIDIFile
+from mingus.core.progressions import to_chords
+
+####################################################################
+# Data
+####################################################################
+
+####################################################################
+# Main
+####################################################################
+
+class Chords2Midi(object):
+    """
+    Read CLI input, create MIDI files.
+
+    """
+
+    def handle(self, argv=None):
+        """
+        Main function.
+
+        Parses command, load settings and dispatches accordingly.
+
+        """
+        help_message = "Please supply chord progression!. See --help for more options."
+        parser = argparse.ArgumentParser(description='chords2midi - Create MIDI files from written chord progressions.\n')
+        parser.add_argument('progression', metavar='U', type=str, nargs='*', help=help_message)
+        parser.add_argument('-i', '--install', action='store_true', help='Install into Ableton directory', default=False)
+        parser.add_argument('-b', '--bpm', type=int, default=160, help='Set the BPM (default 160)')
+        parser.add_argument('-t', '--octave', type=int, default=4, help='Set the octave (default 4)')
+        parser.add_argument('-k', '--key', type=str, default='C', help='Set the key (default C)')
+        parser.add_argument('-d', '--duration', type=float, default=1.0, help='Set the chord duraction (default 1)')
+        parser.add_argument('-v', '--version', action='store_true', default=False,
+            help='Display the current version of chords2midi')
+
+        args = parser.parse_args(argv)
+        self.vargs = vars(args)
+
+        if self.vargs['version']:
+            version = pkg_resources.require("chords2midi")[0].version
+            print(version)
+            return
+
+        # Support `c2m I III V and `c2m I,III,V` formats.
+        if len(self.vargs['progression']) < 1:
+            print("You need to supply a progression! (ex I V vi IV)")
+            return
+        if len(self.vargs['progression']) < 2:
+            progression = self.vargs['progression'][0].split(',')
+        else:
+            progression = self.vargs['progression']
+
+        track    = 0
+        channel  = 0
+        ttime     = 0
+        duration = self.vargs['duration'] # In beats
+        tempo    = self.vargs['bpm']   # In BPM
+        volume   = 100  # 0-127, as per the MIDI standard
+
+        midi = MIDIFile(1)
+        midi.addTempo(track, ttime, tempo)
+
+        bar = 0
+        progression_cords = to_chords(progression, self.vargs['key'])
+        for chord in progression_cords:
+            for note in chord:
+                pitch = pychord.utils.note_to_val(note) + (self.vargs['octave'] * 12)
+                midi.addNote(track, channel, pitch, bar, duration, volume)
+            bar = bar + 1
+
+        filename = self.vargs['key'] + '-' + '-'.join(progression) + '.mid'
+
+        if os.path.exists(filename):
+            filename = self.vargs['key'] + '-' + '-'.join(progression) + '-' + str(int(time.time())) + '.mid'
+
+        with open(filename, "wb") as output_file:
+            midi.writeFile(output_file)
+
+
+def handle(): # pragma: no cover
+    """
+    Main program execution handler.
+    """
+    try:
+        c2m_obj = Chords2Midi()
+        c2m_obj.handle()
+    except (KeyboardInterrupt, SystemExit): # pragma: no cover
+        return
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__': # pragma: no cover
+    handle()
